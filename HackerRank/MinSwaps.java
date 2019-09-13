6a2bc5236e2fdbae29c48ad49029503e0d96dd06
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-14 02:11:54 +0530

    Create MinSwaps.java

---
+++ HackerRank/MinSwaps.java
@@ -0,0 +1,18 @@
+/* Quest 17) Min number of swaps required to sort an array
+ * */
+
+package HackerRankDS;
+
+import java.util.*;
+//import javafx.util.Pair;
+
+public class MinSwaps {
+	
+	public void getMinSwaps(int []arr){
+		
+	} 
+	
+	public static void main(String []args){
+		
+	}
+}
