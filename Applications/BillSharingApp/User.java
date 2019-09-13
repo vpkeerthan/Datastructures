b1723d368150c0c484edf75af8743e1cebf2c4d1
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-13 23:19:22 +0530

    Create User.java

---
+++ Applications/BillSharingApp/User.java
@@ -0,0 +1,11 @@
+package BillSharingApp;
+
+public class User {
+	String userName;
+	User(String userName){
+		this.userName = userName;
+	}
+	public String getUserName(){
+		return this.userName;
+	}
+}
